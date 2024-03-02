51b16e0b4a8d976ce08bc1db1216a5482da49cca
grad48 <152669027+grad48@users.noreply.github.com>
2024-03-03 00:43:02 +0200

    Update antichangeinfobox.js

--- script/antichangeinfobox.js
+++ script/antichangeinfobox.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports.config = {
-	name: "antichangeinfobox",
+	name: "حماية",
 	version: "1.8",
 	credits: "NTKhang",
 	cooldown: 5,
@@ -10,19 +10,19 @@ module.exports.config = {
 	aliases: ["anti-change"],
 	description: {
 		vi: "Chống đổi thông tin box chat",
-		en: "Anti change info box"
+		en: "مربع معلومات مكافحة التغيير"
 	},
 	usage: {
-		vi: "   {pn} avt [on | off]: chống đổi avatar box chat"
-			+ "\n   {pn} name [on | off]: chống đổi tên box chat"
-			+ "\n   {pn} nickname [on | off]: chống đổi nickname trong box chat"
-			+ "\n   {pn} theme [on | off]: chống đổi theme (chủ đề) box chat"
-			+ "\n   {pn} emoji [on | off]: chống đổi trạng emoji box chat",
-		en: "   {pn} avt [on | off]: anti change avatar box chat"
-			+ "\n   {pn} name [on | off]: anti change name box chat"
-			+ "\n   {pn} nickname [on | off]: anti change nickname in box chat"
-			+ "\n   {pn} theme [on | off]: anti change theme (chủ đề)! box chat"
-			+ "\n   {pn} emoji [on | off]: anti change emoji box chat"
+		vi: "   {pn} صورة [on | off]: chống đổi avatar box chat"
+			+ "\n   {pn} اسم [on | off]: chống đổi tên box chat"
+			+ "\n   {pn} كنيات [on | off]: chống đổi nickname trong box chat"
+			+ "\n   {pn} ثيم [on | off]: chống đổi theme (chủ đề) box chat"
+			+ "\n   {pn} اموجي [on | off]: chống đổi trạng emoji box chat",
+		en: "   {pn} صورة [on | off]: anti change avatar box chat"
+			+ "\n   {pn} اسم [on | off]: anti change name box chat"
+			+ "\n   {pn} كنيه [on | off]: anti change nickname in box chat"
+			+ "\n   {pn} ثيم [on | off]: anti change theme (chủ đề)! box chat"
+			+ "\n   {pn} اموجي [on | off]: anti change emoji box chat"
 	}
 };
 
@@ -47,15 +47,15 @@ const langs = {
 		antiChangeEmojiAlreadyOn: "Hiện tại box chat của bạn đang bật chức năng cấm thành viên đổi emoji"
 	},
 	en: {
-		antiChangeAvatarOn: "Turn on anti change avatar box chat",
-		antiChangeAvatarOff: "Turn off anti change avatar box chat",
-		missingAvt: "You have not set avatar for box chat",
-		antiChangeNameOn: "Turn on anti change name box chat",
-		antiChangeNameOff: "Turn off anti change name box chat",
-		antiChangeNicknameOn: "Turn on anti change nickname box chat",
-		antiChangeNicknameOff: "Turn off anti change nickname box chat",
-		antiChangeThemeOn: "Turn on anti change theme box chat",
-		antiChangeThemeOff: "Turn off anti change theme box chat",
+		antiChangeAvatarOn: "قم بتشغيل صندوق الدردشة الرمزية المضاد للتغيير",
+		antiChangeAvatarOff: "قم بإيقاف تشغيل صندوق الدردشة الرمزية المضاد للتغيير",
+		missingAvt: "لم تقم بتعيين الصورة الرمزية للدردشة مربع ",
+		antiChangeNameOn: "قم بتشغيل الدردشة في مربع مكافحة تغيير الاسم",
+		antiChangeNameOff: "قم بإيقاف تشغيل مربع الدردشة المضاد لتغيير الاسم ",
+		antiChangeNicknameOn: "قم بتشغيل الدردشة في صندوق مكافحة تغيير اللقب ",
+		antiChangeNicknameOff: "قم بإيقاف تشغيل صندوق الدردشة المضاد لتغيير اللقب ",
+		antiChangeThemeOn: "قم بتشغيل مربع الدردشة المضاد للتغيير ",
+		antiChangeThemeOff: "قم بإيقاف تشغيل الدردشة في مربع مكافحة التغيير ",
 		antiChangeEmojiOn: "Turn on anti change emoji box chat",
 		antiChangeEmojiOff: "Turn off anti change emoji box chat",
 		antiChangeAvatarAlreadyOn: "Your box chat is currently on anti change avatar",
@@ -83,7 +83,7 @@ module.exports.run = async function ({ message, event, args }) {
 	}
 	switch (args[0]) {
 		case "avt":
-		case "avatar":
+		case "صورة":
 		case "image": {
 			if (!fs.existsSync(`./data/${threadID}_imageSrc.json`))
 				return message.reply(langs[getLang()]["missingAvt"]);
@@ -92,22 +92,22 @@ module.exports.run = async function ({ message, event, args }) {
 			await checkAndSaveData("avatar", newImageSrc.image.url);
 			break;
 		}
-		case "name": {
+		case "اسم": {
 			const threadName = JSON.parse(fs.readFileSync(`./data/${threadID}_threadName.json`, 'utf8'));
 			await checkAndSaveData("name", threadName);
 			break;
 		}
-		case "nickname": {
+		case "كنيه": {
 			const members = JSON.parse(fs.readFileSync(`./data/${threadID}_members.json`, 'utf8'));
 			await checkAndSaveData("nickname", members.map(user => ({ [user.userID]: user.nickname })).reduce((a, b) => ({ ...a, ...b }), {}));
 			break;
 		}
-		case "theme": {
+		case "ثيم": {
 			const threadThemeID = JSON.parse(fs.readFileSync(`./data/${threadID}_threadThemeID.json`, 'utf8'));
 			await checkAndSaveData("theme", threadThemeID);
 			break;
 		}
-		case "emoji": {
+		case "اموجي": {
 			const emoji = JSON.parse(fs.readFileSync(`./data/${threadID}_emoji.json`, 'utf8'));
 			await checkAndSaveData("emoji", emoji);
 			break;
